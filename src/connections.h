1fa9cef9c546f791c0adc4485241f0564b026914
Melissa Castillo <mcast052@hammer.cs.ucr.edu>
2015-10-28 17:34:17 -0700

    Fixed some errors

--- src/connections.h
+++ src/connections.h
@@ -3,13 +3,14 @@
 #include <unistd.h>
 #include <sys/types.h> 
 #include <sys/wait.h> 
+#include <stdio.h> 
 
 using namespace std; 
 class ABC //Abstract Base Class
 {
     public: 
-        virtual void bool execute() = 0; //pure virtual function
-}
+        virtual void execute() = 0; //pure virtual function
+};
 
 class Connectors: private ABC
 {
@@ -26,56 +27,25 @@ class Connectors: private ABC
             return prevstate;
         }
         virtual void execute() 
-        {
-            pid_t c_pid = fork(); //Child's process ID
-            pid_t pid; //Parent's process ID  
-            int status; //Will indicate the status of the child process 
-            
-            if(c_pid < 0) 
-            {
-               perror("Forking failed");
-               set_prevstate(0);    
-               return; 
-            }
-            else if(c_pid == 0) 
-            {
-               //If fork() == 0, this fork is the child process
-               execvp(argv[0], argv); 
-               perror("execvp failed in child");
-               set_prevstate(0); 
-               //execvp, if successful, never returns
-            } 
-            else 
-            {
-                if( (pid = waitpid(c_pid,&status) < 0) 
-                { 
-                    perror("ERROR");
-                    //if waitpid returns -1, there was an ERROR 
-                    set_prevstate(0);
-                    return;  
-                }
-                //else, child process if finished and the parent can continue
-                //the rest of its process. 
-                set_prevstate(1); 
-                return; 
-               //return 1 == Coommand has been executed
-           }
-       }
+        {}
+
 }; 
 
 
 class Semicolon: private Connectors
 {
     private: 
-        char * argv[100];
+        char * argv[];
     
     public:         
         Semicolon(bool prev, const vector<string> &args)
         {
             set_prevstate(prev);
-            for(unsigned int i = 0; i < args.size(); i++)
+            unsigned int i = 0; 
+            for(i = 0; i < args.size(); i++)
             {
-               argv[i] = args.at(i); 
+               string cmd = args.at(i); 
+               argv[i] = cmd; 
             }
             argv[i] = NULL;  
         } 
@@ -104,7 +74,7 @@ class Semicolon: private Connectors
             } 
             else 
             {
-                if( (pid = waitpid(c_pid, &status) < 0) 
+                if( (pid = waitpid(c_pid, &status, 0) < 0) 
                 { 
                     perror("ERROR");
                     //if waitpid returns -1, there was an ERROR 
@@ -129,8 +99,9 @@ class AND: private Connectors
     public:
         AND(bool prev, const vector<string> & args)
         {
-            set_prevstate(prev); 
-            for(unsigned int i = 0; i < args.size(); i++)
+            set_prevstate(prev);
+            unsigned int i = 0;  
+            for(i = 0; i < args.size(); i++)
             {
                argv[i] = args.at(i); 
             }
@@ -141,8 +112,40 @@ class AND: private Connectors
             if(get_prevstate() == 0)
                 return; 
             else
-                Connectors::execute(); 
-                //Execute command (only does when previous command succeeded)                
+           {
+               pid_t c_pid = fork(); //Child's process ID
+               pid_t pid; //Parent's process ID  
+               int status; //Will indicate the status of the child process 
+            
+               if(c_pid < 0) 
+               {
+                   perror("Forking failed");
+                   set_prevstate(0);    
+                   return; 
+                }   
+               else if(c_pid == 0) 
+               {
+                   //If fork() == 0, this fork is the child process
+                   execvp(argv[0], argv); 
+                   perror("execvp failed in child");
+                   set_prevstate(0); 
+                   //execvp, if successful, never returns
+               } 
+               else 
+               {
+                   if(pid = waitpid(c_pid, &status, 0) < 0) 
+                   { 
+                       perror("ERROR");
+                       //if waitpid returns -1, there was an ERROR 
+                       set_prevstate(0);
+                       return;  
+                   }
+                   //else, child process if finished and the parent can continue
+                   //the rest of its process. 
+                   set_prevstate(1); 
+                   return; 
+                   //return 1 == Coommand has been executed
+           }                 
         }
 }; 
 
@@ -154,8 +157,9 @@ class OR: private Connectors
     public:
         OR(bool prev, const vector<string> &args)
         {
-            Connectors::set_prevstate(prev); 
-            for(unsigned int i = 0; i < args.size(); i++)
+            Connectors::set_prevstate(prev);
+            unsigned int i = 0;  
+            for(i = 0; i < args.size(); i++)
             {
                argv[i] = args.at(i); 
             }
@@ -166,7 +170,42 @@ class OR: private Connectors
             if(get_prevstate() == 1)
                 return; 
             else
-                Connectors::execute();  
+            { 
                 //Execute command (only when previous command fails
+                pid_t c_pid = fork(); //Child's process ID
+                pid_t pid; //Parent's process ID  
+                int status; //Will indicate the status of the child process 
+            
+                if(c_pid < 0) 
+                {
+                    perror("Forking failed");
+                    set_prevstate(0);    
+                    return; 
+                }   
+                else if(c_pid == 0) 
+                {
+                    //If fork() == 0, this fork is the child process
+                    execvp(argv[0], argv); 
+                    perror("execvp failed in child");
+                    set_prevstate(0); 
+                    //execvp, if successful, never returns
+                } 
+                else 
+                {
+                    if(pid = waitpid(c_pid, &status, 0) < 0) 
+                    { 
+                        perror("ERROR");
+                        //if waitpid returns -1, there was an ERROR 
+                        set_prevstate(0);
+                        return;  
+                    }
+                    //else, child process if finished and the parent can continue
+                    //the rest of its process. 
+                    set_prevstate(1); 
+                    return; 
+                    //return 1 == Coommand has been executed
+                } 
+            }
+
         }
 };
