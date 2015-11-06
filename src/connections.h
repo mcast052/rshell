50f3a527d8641172cbdcb1c2854fd147bdfb2cc8
Melissa Castillo <mcast052@hammer.cs.ucr.edu>
2015-11-06 00:44:54 -0800

    Updated

--- src/connections.h
+++ src/connections.h
@@ -18,9 +18,18 @@ class ABC //Abstract Base Class
 class Connectors: public ABC
 {
     private:
-        bool prevstate;  
+        bool prevstate;
+        bool exitbool;          
         //Will hold value stating if prev state passed or failed 
     public: 
+        void set_exit(bool exbool)
+        {
+            exitbool = exbool; 
+        }
+        bool get_exit()
+        {
+            return exitbool; 
+        }
         void set_prevstate(bool prev)
         {
             prevstate = prev; 
@@ -40,14 +49,14 @@ class Semicolon_Connector: public Connectors
     private: 
         const char * argv[25];
         string first; 
-        bool exitbool; 
+
     public:         
         Semicolon_Connector(bool prev, const vector<string> &args)
         {
             set_prevstate(prev);
-            first = args.at(0); 
-            exitbool = 0; 
-            
+            first = args.at(0);  
+            set_exit(0); 
+
             unsigned int i = 0; 
             for(i = 0; i < args.size(); i++)
             {
@@ -57,61 +66,59 @@ class Semicolon_Connector: public Connectors
         } 
         virtual void execute() 
         {
-            string exitcmd = "exit"; 
-            if(first.compare(exitcmd) == 0)
-            {
-                cout << "Now exiting the shell" << endl; 
-                exitbool = 1; 
-                return;  
-            }           
+           string exitcmd = "exit"; 
+           if(first.compare(exitcmd) == 0)
+           {   
+               set_exit(1); 
+               return; 
+           }           
 
-            //Always executes, no matter if the previous 
-            //command failed or passed
-            pid_t c_pid = fork(); //Child's process ID
-            pid_t pid; //Parent's process ID  
-            int status; 
-
-               if(c_pid < 0) 
-               {
-                   perror("Forking failed");
-                   set_prevstate(0);    
-                   return; 
-                }   
-               else if(c_pid == 0) 
+           //Always executes, no matter if the previous 
+           //command failed or passed
+           pid_t c_pid = fork(); //Child's process ID
+           pid_t pid; //Parent's process ID  
+           int status; 
+        
+           if(c_pid < 0) 
+           {
+               perror("Forking failed");
+               set_prevstate(0);    
+               return; 
+           }   
+           else if(c_pid == 0) 
+           { 
+               //If fork() == 0, this fork is the child process
+                int run = execvp(argv[0], const_cast<char * const *>(argv));
+                if( run == -1) 
+                {  
+                    perror("execvp failed in child");
+                    set_prevstate(0);
+                    //return;
+                    exit(127); 
+                }    
+           } 
+           else
+           {
+               if((pid = waitpid(c_pid, &status, 0)) < 0) 
                { 
-                   //If fork() == 0, this fork is the child process
-                    int run = execvp(argv[0], const_cast<char * const *>(argv));
-                    if( run == -1) 
-                    {  
-                        perror("execvp failed in child");
-                        set_prevstate(0);
-                        //return;
-                        exit(127); 
-                    }    
-               } 
-               else 
-               {
-                   if((pid = waitpid(c_pid, &status, 0)) < 0) 
-                    { 
-                        perror("ERROR while waiting");
-                        //if waitpid returns -1, there was an ERROR 
-                        set_prevstate(0);
-                        return;  
-                    }
-                    //else, child process if finished and the parent can continue
-                    //the rest of its process.
-                    if(status == 0) 
-                    { 
-                        set_prevstate(1); 
-                        return;
-                    }
-                    else if(status != 0) 
-                    {
-                        set_prevstate(0); 
-                        return; 
-                    }
+                   perror("ERROR while waiting");
+                   //if waitpid returns -1, there was an ERROR 
+                   set_prevstate(0);
+                   return;  
                }
-
+               //else, child process if finished and the parent can continue
+               //the rest of its process.
+               if(status == 0) 
+               { 
+                   set_prevstate(1); 
+                   return;
+               }
+               else if(status != 0)
+               {
+                   set_prevstate(0); 
+                   return;
+               }
+           }
         } 
 };
 
@@ -119,15 +126,14 @@ class AND_Connector: public Connectors
 {
     private: 
         const char * argv[25];
-        string first;
-        bool exitbool;  
+        string first; 
 
     public:
         AND_Connector(bool prev, const vector<string> & args)
         {
             set_prevstate(prev);
             first = args.at(0); 
-            exitbool = 0; 
+            set_exit(0); 
 
             unsigned int i = 0;   
             for(i = 0; i < args.size(); i++)
@@ -138,22 +144,18 @@ class AND_Connector: public Connectors
         }
         virtual void execute()
         {
-            string exitcheck = "exit";
-            if(first.compare(exitcheck) == 0)
-            {
-                cout << "Now exiting" << endl; 
-                exitbool = 1;
-                return;  
-            }
-
-
             if(get_prevstate() == 0)
-            {
-                cout << "Previous failed" << endl; 
-                return;
+            { 
+                return; 
             } 
             else
             {
+               string exitcheck = "exit";
+               if(first.compare(exitcheck) == 0)
+               { 
+                   set_exit(1); 
+                   return;
+               }
                pid_t c_pid = fork(); //Child's process ID
                pid_t pid; //Parent's process ID  
                int status; //Will indicate the status of the child process
@@ -192,11 +194,12 @@ class AND_Connector: public Connectors
                         set_prevstate(1); 
                         return; 
                     }
-                    else if(status != 0) 
-                    {
+                    else if(status != 0)
+                    {    
                         set_prevstate(0); 
-                        return;  
-                    }
+                        return;
+                    }  
+                    
                }
             }
            
@@ -207,43 +210,36 @@ class OR_Connector: public Connectors
 {
     private: 
         const char * argv[25]; 
-        string first;
-        bool exitbool;  
+        string first;  
 
     public:
         OR_Connector(bool prev, const vector<string> &args)
         {
             first = args.at(0); 
-            set_prevstate(prev);
-            exitbool = 0; 
+            set_prevstate(prev); 
+            set_exit(0); 
 
             unsigned int i = 0;  
             for(i = 0; i < args.size(); i++)
             {
-              argv[i] = args.at(i).c_str(); 
-              //string cmd = args.at(i); 
-              //argv[i] = &args[i]; 
+              argv[i] = args.at(i).c_str();  
             }
             argv[i] = NULL;  
         } 
         virtual void execute()
         {
-            string exitcheck = "exit";
-            if(first.compare(exitcheck) == 0)
-            {
-                cout << "Now exiting" << endl;
-                exitbool = 1;  
-                return; 
-            }
-
-
             if(get_prevstate() == 1)
-            {
-                cout << "Previous succeeded" << endl; 
-                return;
+            { 
+                return; 
             } 
             else
             { 
+                string exitcheck = "exit";
+                if(first.compare(exitcheck) == 0)
+                { 
+                    set_exit(1); 
+                    return;
+                }
                 //Execute command (only when previous command fails
                 pid_t c_pid = fork(); //Child's process ID
                 pid_t pid; //Parent's process ID  
@@ -262,7 +258,6 @@ class OR_Connector: public Connectors
                     {  
                         perror("execvp failed in child");
                         set_prevstate(0);
-                        //return;
                         exit(127); 
                     }                     
                 }
@@ -275,18 +270,17 @@ class OR_Connector: public Connectors
                         set_prevstate(0);
                         return;  
                     }
-
                     //else, child process if finished and the parent can continue
                     //the rest of its process.
                     if(status == 0) 
                     { 
                         set_prevstate(1); 
                         return;
-                    }   
+                    }
                     else if(status != 0) 
-                    {
+                    {   
                         set_prevstate(0); 
-                        exit(127); 
+                        return; 
                     }
                 }
             }   
