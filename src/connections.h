39849921e39c9bbb55ee8af3d3fd1a94b88dc41d
Melissa Castillo <mcast052@hammer.cs.ucr.edu>
2015-11-19 23:56:19 -0800

    Fixed errors in Paren class

--- src/connections.h
+++ src/connections.h
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <sys/types.h> 
 #include <sys/wait.h> 
+#include <sys/stat.h>
 #include <stdio.h>
 #include <cstdlib>
 
@@ -18,10 +19,19 @@ class ABC //Abstract Base Class
 class Connectors: public ABC
 {
     private:
+        bool error_check; 
         bool prevstate;
         bool exitbool;          
         //Will hold value stating if prev state passed or failed 
     public: 
+        bool get_error()
+        {
+            return error_check;
+        }
+        void set_error(bool err)
+        {
+            error_check = err; 
+        }
         void set_exit(bool exbool)
         {
             exitbool = exbool; 
@@ -49,13 +59,14 @@ class Semicolon_Connector: public Connectors
     private: 
         const char * argv[25];
         string first; 
-
+        vector<string> test; 
     public:         
         Semicolon_Connector(bool prev, const vector<string> &args)
         {
             set_prevstate(prev);
             first = args.at(0);  
             set_exit(0); 
+            test = args; 
 
             unsigned int i = 0; 
             for(i = 0; i < args.size(); i++)
@@ -67,11 +78,64 @@ class Semicolon_Connector: public Connectors
         virtual void execute() 
         {
            string exitcmd = "exit"; 
+           string test_check = "test"; 
            if(first.compare(exitcmd) == 0)
            {   
                set_exit(1); 
                return; 
            }           
+           else if(first.compare(test_check) == 0)
+           {
+               struct stat sb; 
+               if(stat(argv[1], &sb) == -1) 
+               {
+                   perror("stat"); 
+                   set_prevstate(0); 
+                   return; 
+               }
+
+               bool if_exists = false; 
+               bool if_reg = false; 
+               bool if_dir = false; 
+               for(unsigned int i = 0; i < test.size(); i++)
+               {
+                   if(test.at(i) == "-e")
+                   {
+                       if_exists = true;  
+                   }
+                   else if(test.at(i) == "-f")
+                   {
+                       if_reg = true;  
+                   }
+                   else if(test.at(i) == "-d")
+                   {
+                       if_dir = true;  
+                   }
+               } 
+
+               if(if_exists)
+               {
+                   set_prevstate(1); 
+               }
+               if(if_reg && S_ISREG(sb.st_mode))
+               {
+                   set_prevstate(1); 
+               }
+               else 
+               {
+                   set_prevstate(0); 
+               }
+               if(if_dir && S_ISDIR(sb.st_mode))
+               {
+                   set_prevstate(1); 
+               }
+               else
+               {
+                   set_prevstate(0); 
+               }
+
+               return; 
+           }               
 
            //Always executes, no matter if the previous 
            //command failed or passed
@@ -287,25 +351,73 @@ class OR_Connector: public Connectors
         }
 };
 
-class Paren: class Connectors
+class Paren: public Connectors
 {
     private:  
-       vector<Connector *> v;   
+       vector<Connectors *> v;   
        bool is_and; 
-       bool is_or; 
-       bool error_check; 
+       bool is_or;  
 
     public: 
        Paren(bool is_and_1, bool is_or_1, 
-             bool prev, vector<string> commands)
+             bool prev, vector<string> com)
        { 
             is_and = is_and_1; 
             is_or = is_or_1; 
             set_prevstate(prev); 
 
-            error_check = false; 
+            set_error(0);  
+            
+            vector< vector<string> > commands;
+            vector<string> indivcommand;
+            bool ifHash = false;   
+            for(unsigned int i = 0; i < com.size(); i++)
+            {   
+                if(com.at(i) == "#")
+                {
+                    if(indivcommand.size() != 0)
+                    {
+                        commands.push_back(indivcommand); 
+                        ifHash = true; 
+                        break; 
+                    }
+                    else
+                    {
+                        ifHash = true; 
+                        break; 
+                    }
+                }
+                if(com.at(i) == ";")
+                {
+                    if(indivcommand.size() != 0)
+                    {
+                        commands.push_back(indivcommand); 
+                    }
+                    indivcommand.clear(); 
+                }
+                else if(com.at(i) == "&&")
+                {
+                    if(indivcommand.size() != 0)
+                    {
+                        commands.push_back(indivcommand); 
+                    }
+                    indivcommand.clear(); 
+                }
+                else if(com.at(i) == "||")
+                {
+                    if(indivcommand.size() != 0)
+                    {
+                        commands.push_back(indivcommand); 
+                    }
+                    indivcommand.clear(); 
+                }
+            }
+            if(!indivcommand.empty() && ifHash)
+            {
+                commands.push_back(indivcommand); 
+            }
 
-            while(!error_check)
+            while(!get_error())
             {
                 unsigned int j = commands.size() - 1;   
                 for(unsigned int i = 0; i < commands.size(); i++) 
@@ -314,19 +426,19 @@ class Paren: class Connectors
                     if(i == 0 && commands.at(i).at(0) == ";") 
                     {
                         cout << "Syntax error near unexpected token ';'" << endl;
-                        error_check = true;
+                        set_error(1); 
                         break; 
                     }
                     else if(i == 0 && commands.at(i).at(0) == "||") 
                     {
                         cout << "Syntax error near unexpected token \"||\"" << endl; 
-                        error_check = true;
+                        set_error(1); 
                         break;
                     }
                     else if(i == 0 && commands.at(i).at(0) == "&&") 
                     {
                         cout << "Syntax error near unexpected token \"&&\"" << endl; 
-                        error_check = true;
+                        set_error(1); 
                         break;
                     }
 
@@ -340,7 +452,7 @@ class Paren: class Connectors
                         if(i == j)
                         {
                             cout << "Syntax error" << endl;
-                            error_check  = true;
+                            set_error(1); 
                             break;
                         } 
                         v.push_back(new Semicolon_Connector(0, commands.at(i + 1)));  
@@ -351,7 +463,7 @@ class Paren: class Connectors
                         {
 
                             cout << "Syntax error" << endl;
-                            error_check = true;
+                            set_error(1); 
                             break; 
                         }
                         v.push_back(new AND_Connector(0, commands.at(i + 1) ));   
@@ -361,7 +473,7 @@ class Paren: class Connectors
                         if(i == j) 
                         {
                             cout << "Syntax error" << endl;
-                            error_check = true;
+                            set_error(1); 
                             break; 
                         }
                         v.push_back(new OR_Connector (0, commands.at(i + 1) ));   
@@ -372,23 +484,19 @@ class Paren: class Connectors
 
        virtual void execute()
        {
-            if(is_or && get_prevstate)
+            if(is_or && this->get_prevstate())
             {
                 return; 
             }
-            else if(is_and && !get_prevstate)
+            else if(is_and && !this->get_prevstate())
             {
                 return; 
             }
 
             for(unsigned int i = 0; i < v.size(); i++)
             {
-                v.at(i)->execute;       
+                v.at(i)->execute();       
             }
             set_prevstate(v.at(v.size() - 1)->get_prevstate()); 
-       }
-       bool get_error()
-       {
-           return error_check; 
        }
 };
